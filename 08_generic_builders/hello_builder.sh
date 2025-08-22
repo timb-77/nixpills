31a9276a44c86ee2b339022f1ddfa03a968b7f4f
Tim Bernwald <mail@timbernwald.de>
2025-08-22 16:31:05 +0200

    08_generic_builders - 1st version

---
+++ 08_generic_builders/hello_builder.sh
@@ -0,0 +1,8 @@
+
+export PATH="$gnutar/bin:$gcc/bin:$gnumake/bin:$coreutils/bin:$gawk/bin:$gzip/bin:$gnugrep/bin:$gnused/bin:$bintools/bin"
+tar -xzf $src
+cd hello-2.12.1
+./configure --prefic=$out
+make
+make install
+
