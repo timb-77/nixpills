094546f6683a86001c89543e7101f3588833c41f
Tim Bernwald <mail@timbernwald.de>
2025-08-24 19:04:42 +0200

    Add 2nd (more generic autotools) version

---
+++ 08_generic_builders/2nd_version/builder.sh
@@ -0,0 +1,19 @@
+set -e
+unset PATH
+for p in ${buildInputs}; do
+    export PATH=${p}/bin${PATH:+:}${PATH}
+done
+
+tar -xzf ${src}
+
+for d in *; do
+    if [ -d "${d}" ]; then
+        cd "${d}"
+        break;
+    fi
+done
+
+./configure --prefix=${out}
+make
+make install
+
